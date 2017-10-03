bdd992c4600013d2dd7c8484e1b194c50c5d7dc5
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-03 13:17:53 +0000

    automatically backed up by learn

--- scratch.js
+++ scratch.js
@@ -2,31 +2,31 @@ function find(array, criteriaFn) {
 
   let current = array
   let next
- 
+
   while (current) {
     if (criteriaFn(current)) {
       return current
     }
-    
+
     // Put child arrays into next
     if (Array.isArray(current)) {
       for (let i = 0; i < current.length; i++) {
         next.push(current[i])
       }
-      
+
       current = next.shift()
-      
+
     } else if ((typeof current)) === "object") {
       nextObj = Object.assign(current)
-      //make array of 
+      //make array of
       next = Object.keys(nextObj)
 
       current = next.shift()
       current = {[next.shift()]: person[keysArray[0]]}
     }
-    
+
 
   }
- 
+
   return null
 }
