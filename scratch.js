c98037c1fec2bc816ba5f6aca194a7c951b80c64
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-03 13:17:49 +0000

    automatically backed up by learn

---
+++ scratch.js
@@ -0,0 +1,32 @@
+function find(array, criteriaFn) {
+
+  let current = array
+  let next
+ 
+  while (current) {
+    if (criteriaFn(current)) {
+      return current
+    }
+    
+    // Put child arrays into next
+    if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i])
+      }
+      
+      current = next.shift()
+      
+    } else if ((typeof current)) === "object") {
+      nextObj = Object.assign(current)
+      //make array of 
+      next = Object.keys(nextObj)
+
+      current = next.shift()
+      current = {[next.shift()]: person[keysArray[0]]}
+    }
+    
+
+  }
+ 
+  return null
+}
